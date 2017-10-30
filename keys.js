77ef9ca28fa3dc9b6a227ca7e15414281859f3e2
Deva <deva.palasingam@gmail.com>
2017-10-30 17:41:58 -0500

    finished twitter command

---
+++ keys.js
@@ -0,0 +1,11 @@
+
+var twitterKeys = {
+  consumer_key: 'szvWyAofzp7lx6bOgbbtlfWBd',
+  consumer_secret: 'INfan0siFkAIsbctYL7YJGKvIOwJgY80FMfP692aopx4ujPfyL',
+  access_token_key: '924690386844618752-7ejmAme2PVfiyUFiVdvlmjs3GddWsQK',
+  access_token_secret: 'muQvit785ZsglJ446vc1hOxkKsAF7gddiDKp2pGKyvLHb',
+}
+
+module.exports = {
+	twitterKeys: twitterKeys
+};
---
+++ liri.js
@@ -0,0 +1,57 @@
+var importKeys = require('./keys.js');
+
+var Twitter = require('twitter');
+var inquirer = require("inquirer");
+
+
+
+inquirer.prompt([
+	{
+		type: "input",
+		message: "What can I do for you?",
+		name: "userInput"
+	}
+]).then(function(inquirerResponse) {
+	if (inquirerResponse.userInput === "my-tweets") {
+		lastTweets();
+	}
+	else {
+		console.log("I'm sorry, here are your options:")
+		console.log("my-tweets");
+	}
+});
+
+
+
+
+
+
+
+
+
+function lastTweets() {
+
+var client = new Twitter({
+		consumer_key: importKeys.twitterKeys.consumer_key,
+		consumer_secret: importKeys.twitterKeys.consumer_secret,
+		access_token_key: importKeys.twitterKeys.access_token_key,
+		access_token_secret: importKeys.twitterKeys.access_token_secret
+	});
+
+
+var params = {screen_name: 'ChickenTrashcan'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    
+
+  	for(var i = 0; i < 20; i ++) {
+  		console.log("");
+  		console.log(tweets[i].text);
+  	}
+
+
+    debugger;
+  }
+});
+
+}
